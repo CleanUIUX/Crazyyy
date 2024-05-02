14f90caa45d5e1420b87beef6f2cf94b0f5d6fdc
SeungChan-Roh <ox043440@gmail.com>
2024-05-02 22:49:19 +0900

    스크롤 시 헤더 배경색 추가

---
+++ blog/src/components/Contents.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Contents(){
+    return(
+        <StyledWrap></StyledWrap>
+    )
+}
+
+const StyledWrap = styled.div`
+    height:100vh;
+`
+
+export default Contents;
--- blog/src/components/Header.jsx
+++ blog/src/components/Header.jsx
@@ -1,19 +1,39 @@
 import styled from "styled-components";
 import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
 
 function Header() {
+
+    const [scrollY, setScrollY ] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    })
+
     return(
         <Router>
-            <StyledHeader>
-                <h1>Crazyyy</h1>
-                <StyledGnb>
-                    <StyledItem><Link to="/">list1</Link></StyledItem>
-                    <StyledItem><Link to="/">list2</Link></StyledItem>
-                </StyledGnb>
-                <StyledBox>
-                    <SearchInput type="text" />
-                    {/* <button className="btn_del"></button> */}
-                </StyledBox>
+            <StyledHeader style={{ backgroundColor: scrollY > 0 ? 'red' : 'transparent' }}>
+                <StyleInner>
+                    <StyleLogo>Crazyyy</StyleLogo>
+                    <StyledNav>
+                        <StyledUl>    
+                            <StyledItem><StyledLink to="/">list1</StyledLink></StyledItem>
+                            <StyledItem><StyledLink to="/">list2</StyledLink></StyledItem>
+                        </StyledUl>
+                    </StyledNav>
+                    <StyledBox>
+                        <SearchInput type="text" />
+                        {/* <button className="btn_del"></button> */}
+                    </StyledBox>
+                </StyleInner>
             </StyledHeader>
         </Router>
     )
@@ -21,18 +41,44 @@ function Header() {
 
 const StyledHeader = styled.header`
     width:100%;
-    height:200px;
-    background:#555;
+    height:100px;
+    border-bottom:1px solid #222 ;
 `;
-const StyledGnb = styled.ul`
-    display:flex;
+const StyleInner = styled.div`
+    position:relative;
+    margin:0 100px;
+`
+const StyleLogo = styled.h1`
+    position:absolute;
+    top:0;
+    left:0;
+`
+const StyledNav = styled.nav`
+    display:block;
 `;
+const StyledUl = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+`
 const StyledItem = styled.li`
-    border:1px solid #222;
+    height:100%;
 `;
-
+const StyledLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    padding: 0 35px;
+    height: 100%;
+    color: #222;
+    text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.60);
+    
+`;
 const StyledBox = styled.div`
-    float:right;
+    position:absolute;
+    top:0;
+    right:0;
 `;
 const SearchInput = styled.input`
     padding: 5px;
