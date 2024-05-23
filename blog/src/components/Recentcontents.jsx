5370b6bb1471a3458721dc12e0f4372ed12be8cc
SeungChan-Roh <ox043440@gmail.com>
2024-05-23 17:45:22 +0900

    update

--- blog/src/components/Recentcontents.jsx
+++ blog/src/components/Recentcontents.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+import { txtlist } from "../data/txtList_data";
+import { txtpost } from "../data/txtPost_data";
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 function Recentcontents() {
 
-    const [thumbsUp, setThumbsUp] = useState({ btn1:0, btn2:0 });
+    const [thumbsUp, setThumbsUp] = useState({ btn1:0, btn2:0, btn3:0 });
     const handleThumbsUp = (button) => {
         setThumbsUp((prevThumbsUp) => ({
             ...prevThumbsUp,
@@ -17,59 +20,95 @@ function Recentcontents() {
                     <Title>최신 글✨</Title>
                     <Description>최신 포스팅을 구경해보세요!</Description>
                     <Content>
-                        <PostBox>
-                            <div>
-                                <img src="" alt="" />
-                            </div>
-                            <TagWrap>
-                                <TagList>
-                                    <TagItem><Tag>태그1</Tag></TagItem>
-                                </TagList>
-                            </TagWrap>
-                            <PostTitle>여기에는 글제목!</PostTitle>
-                            <PostDesc>글내용</PostDesc>
-                            <UserInfo>
-                                <UserImage  src="/img/img2.jpg" alt="" />
-                                <UserName>글쓴이</UserName>
-                                <PostDate>24.05.00</PostDate>
-                            </UserInfo>
-                            <AddOns>
-                                <Comment>
-                                    <CommentCount>3</CommentCount>
-                                </Comment>
-                                <BtnDown onClick={() => handleThumbsUp('btn1')}>👍{thumbsUp.btn1}</BtnDown>
-                            </AddOns>
+                        <PostBox to="/">
+                            {txtpost.map(() => (
+                                <>
+                                <PostImgBox>
+                                    <PostImg src="/img/img3.jpg" alt="" />
+                                </PostImgBox>
+                                <PostTxtBox>
+                                    <TagWrap>
+                                        <TagList>
+                                            <TagItem><Tag>태그1</Tag></TagItem>
+                                        </TagList>
+                                    </TagWrap>
+                                    <PostTitle>여기에는 글제목!</PostTitle>
+                                    <PostDesc>글내용</PostDesc>
+                                    <UserInfo>
+                                        <UserImage  src="/img/img2.jpg" alt="" />
+                                        <UserName>글쓴이</UserName>
+                                        <PostDate>24.05.00</PostDate>
+                                    </UserInfo>
+                                    <AddOns>
+                                        <Comment>
+                                            <CommentCount>📝3</CommentCount>
+                                        </Comment>
+                                        <BtnDown onClick={() => handleThumbsUp('btn1')}>👍{thumbsUp.btn1}</BtnDown>
+                                    </AddOns>
+                                </PostTxtBox>
+                                </>
+                            ))}
                         </PostBox>
-                        <TxtList>
-                            <ImgBox>
-                                <Img src="/img/img1.jpg" alt="" />
-                            </ImgBox>
-                            <TxtBox>
-                                <TagWrap>
-                                    <TagList>
-                                        <TagItem><Tag>태그1</Tag></TagItem>
-                                    </TagList>
-                                </TagWrap>
-                                <TxtTitle>여기에는 글제목!</TxtTitle>
-                                <UserInfo>
-                                    <UserImage  src="/img/img2.jpg" alt="" />
-                                    <UserName>글쓴이</UserName>
-                                    <PostDate>24.05.00</PostDate>
-                                </UserInfo>
-                                <AddOns>
-                                    <Comment>
-                                        <CommentCount>3</CommentCount>
-                                    </Comment>
-                                    <BtnDown onClick={() => handleThumbsUp('btn2')}>👍{thumbsUp.btn2}</BtnDown>
-                                </AddOns>
-                            </TxtBox>
-                        </TxtList>
+                        <Router>
+                            <TxtListWrap>
+                                {txtlist.map((post) => (
+                                    <Link to="/">
+                                        <TxtList>
+                                            <ImgBox>
+                                                <Img src={post.imgSrc} alt="" />
+                                            </ImgBox>
+                                            <TxtBox>
+                                                <TagWrap>
+                                                        <TagList>
+                                                            {post.tags.map((tag, tag_data) => (
+                                                                <TagItem key={tag_data}><Tag>{tag}</Tag></TagItem>
+                                                            ))}
+                                                        </TagList>
+                                                </TagWrap>
+                                                <TxtTitle>{post.title}</TxtTitle>
+                                                <UserInfo>
+                                                    <UserImage  src={post.userImgSrc} alt="" />
+                                                    <UserName>{post.name}</UserName>
+                                                    <PostDate>{post.date}</PostDate>
+                                                </UserInfo>
+                                                <AddOns>
+                                                    <Comment>
+                                                        <CommentCount>📝{post.comments}</CommentCount>
+                                                    </Comment>
+                                                    <BtnDown onClick={() => handleThumbsUp(post.thumbsUpKey)}>👍{thumbsUp[post.thumbsUpKey]}</BtnDown>
+                                                </AddOns>
+                                            </TxtBox>
+                                        </TxtList>
+                                    </Link>
+                                ))}
+                            </TxtListWrap>
+                        </Router>
                     </Content>
                 </Inner>
             </Section>
         </>
     )
 };
+let fadeIn = keyframes`
+from {
+    opacity:0;
+    transform:scale(0.95);
+}
+to {
+    opacity: 1;
+    transform: scale(1);
+}
+`;
+let scaleUp = keyframes`
+from {
+    opacity:0;
+    transform:translateY(5px);
+}
+to {
+    opacity: 1;
+    transform:translateY(0);
+}
+`
 const Section = styled.section`
     padding:30px 0;
 `
@@ -94,11 +133,39 @@ const Content = styled.div`
 `
 const PostBox = styled.div`
     width:50%;
-    margin-right:4px;
-    padding:20px;
+    margin-right:16px;
     border:1px solid #ebebeb;
     border-radius:20px;
     text-align:left;
+`
+const PostImgBox = styled.div`
+    display:flex;
+    -webkit-box-align:center;
+    align-items:center;
+    overflow:hidden;
+    position:relative;
+    width:100%;
+    padding-top:300px;
+    border-radius:20px 20px 0 0;
+`
+const PostImg = styled.img`
+    display:block;
+    position:absolute;
+    top:0;
+    right:0;
+    bottom:0;
+    left:0;
+    max-height:100%;
+    max-width:100%;
+    min-height:100%;
+    min-width:100%;
+    width:0;
+    height:0;
+    margin:auto;
+    object-fit:cover;
+`
+const PostTxtBox = styled.div`
+    padding:32px;
 `
 const PostTitle = styled.p`
     margin-top:16px;
@@ -108,13 +175,24 @@ const PostTitle = styled.p`
 const PostDesc = styled.p`
     margin-top:24px;
 `
+const TxtListWrap = styled.div`
+    display:flex;
+    flex-direction:column;
+`
 const TxtList = styled.div`
     position:relative;
-    width:240px;
-    height:100%;
+    width:360px;
+    margin-left:16px;
+    margin-bottom:8px;
     padding:32px 200px 32px 24px;
     border:1px solid #ebebeb;
     border-radius:20px;
+
+    &:hover {
+        animation: ${scaleUp} 0.5s ease-out;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+    animation: ${fadeIn} 0.5s ease-out;
 `
 const TxtBox = styled.div`
     text-align:left;
@@ -168,16 +246,20 @@ const PostDate = styled.span`
 const AddOns = styled.div`
     display:flex;
     justify-content:space-between;
-    margin-top:16px;
+    margin-top:24px;
 `
 const Comment = styled.div`
     
 `
 const CommentCount = styled.span`
-    
+    padding:4px 10px;
+    font-size:12px;
+    border-radius:10px;
+    background:#efefef;
 `
 const BtnDown  = styled.span`
     margin-right:10px;
+    font-size:14px;
     font-weight:500;
 `
 const ImgBox = styled.div`
@@ -200,5 +282,6 @@ const Img = styled.img`
     min-height: 100%;
     margin: auto;
     object-fit: cover;
+    border-radius:20px;
 `
 export default Recentcontents;
---
+++ blog/src/data/txtList_data.js
@@ -0,0 +1,35 @@
+export const txtlist = [
+    {
+        id: 1,
+        title: "첫 번째 글제목첫 번째 글제목첫 번째 글제목첫 번째 글제목첫 번째 글제목",
+        name: "글쓴이1",
+        date: "2024.05.01",
+        imgSrc: "/img/img1.jpg",
+        userImgSrc: "/img/img2.jpg",
+        tags: ["태그1"],
+        comments: 3,
+        thumbsUpKey: "btn1"
+      },
+      {
+        id: 2,
+        title: "두 번째 글제목",
+        name: "글쓴이2",
+        date: "2024.05.02",
+        imgSrc: "/img/img2.jpg",
+        userImgSrc: "/img/img3.jpg",
+        tags: ["태그2"],
+        comments: 5,
+        thumbsUpKey: "btn2"
+      },
+      {
+        id: 3,
+        title: "세 번째 글제목",
+        name: "글쓴이3",
+        date: "2024.05.03",
+        imgSrc: "/img/img3.jpg",
+        userImgSrc: "/img/img2.jpg",
+        tags: ["태그3"],
+        comments: 6,
+        thumbsUpKey: "btn3"
+      },
+]
